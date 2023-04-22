8a4c6451a02caaa2061e6aef499e88cec9a59ecd
“danielsolaquegit config --global user.email “danielsolaque@pursuit.org <example@email.com>
2023-04-22 18:38:15 -0400

    adding 2

--- green/src/New.js
+++ green/src/New.js
@@ -3,20 +3,36 @@ import "./New.css";
 import { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 
+// id SERIAL PRIMARY KEY,
+//     name TEXT NOT NULL,
+//     electric INT,
+//     gas INT,
+//     oil INT,
+//     car_mileage INT,
+//     flights INT,
+//     recycle_newspaper BOOLEAN,
+//     recycle_aluminum BOOLEAN
 export default function New() {
-// const navigate = useNavigate();    
-const [newScore, setNewScore] = useState({
-  author: "",
-  city: "",
-  created_at: "",
-  electric: "",
-  gas: "",
-  oil: "",
-  car_mileage: "",
-  flights: "",
-  recycle_newspaper: "",
-  recycle_aluminum: "",
-});
+  // const navigate = useNavigate();
+  const [totalScore, setTotalScore] = useState(0);
+  const [newScore, setNewScore] = useState({
+    name: "",
+    electric: "",
+    gas: "",
+    oil: "",
+    car_mileage: "",
+    short_flights: "",
+    long_flights: "",
+    recycle_newspaper: "",
+    recycle_aluminum: "",
+    city: "",
+  });
+
+  // {
+  //   name: ,
+  //   city: ,
+  //   total: ,
+  // }
 
   function handleInputChange(event) {
     setNewScore({
@@ -25,9 +41,41 @@ const [newScore, setNewScore] = useState({
     });
   }
 
+  function handleCheckboxChange(event) {
+    setNewScore({
+      ...newScore,
+      [event.target.id]: event.target.checked,
+    });
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+    let total = 0;
 
+    const computedElectric = Number(newScore.electric) * 105;
+    const computedGas = Number(newScore.gas) * 105;
+    const computedOil = Number(newScore.oil) * 113;
+    const computedCarMileage = Number(newScore.car_mileage) * 79;
+    const computedShortFlights = Number(newScore.short_flights) * 1100;
+    const computedLongFlights = Number(newScore.long_flights) * 4400;
+
+    if (!newScore.recycle_newspaper) {
+      total += 184;
+    }
+
+    if (!newScore.recycle_aluminum) {
+      total += 166;
+    }
+
+    total +=
+      computedElectric +
+      computedGas +
+      computedOil +
+      computedCarMileage +
+      computedLongFlights +
+      computedShortFlights;
+
+    setTotalScore(total);
     // const acumulatedScore = {
     //   ...newScore,
     //   newScore,
@@ -37,6 +85,17 @@ const [newScore, setNewScore] = useState({
     //   navigate("/scores");
     // });
   }
+
+  function handleShareScore() {
+    console.log("Sending data to backend");
+    // Implement backend communication
+    // POST data from newScore to backend
+
+    // fetch(myserver, {
+    //   method: "POST",
+    //   body: JSON.stringify()
+    // })
+  }
   return (
     <div>
       <header className="upsert-form-header">
@@ -44,26 +103,26 @@ const [newScore, setNewScore] = useState({
       </header>
       <form className="upsert-form" onSubmit={handleSubmit}>
         <div className="form-field">
-          <label htmlFor="title">Name:</label>
+          <label htmlFor="name">Name:</label>
           <input
             type="text"
-            id="title"
-            value={newScore.title}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        <div className="form-field">
-          <label htmlFor="author">City:</label>
-          <input
-            type="text"
-            id="author"
+            id="name"
             value={newScore.name}
             onChange={handleInputChange}
           />
         </div>
 
         <div className="form-field">
+          <label htmlFor="city">City:</label>
+          <input
+            type="text"
+            id="city"
+            value={newScore.city}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        {/* <div className="form-field">
           <label htmlFor="category">Today's Date:</label>
           <input
             type="text"
@@ -71,52 +130,116 @@ const [newScore, setNewScore] = useState({
             value={newScore.category}
             onChange={handleInputChange}
           />
-        </div>
+        </div> */}
+
         <h2>Please answer the following questions:</h2>
         <div className="form-field">
-          <label htmlFor="brand">
+          <label htmlFor="electric">
             How much was your electric bill this month?:
           </label>
           <input
-            type="text"
-            id="brand"
-            value={newScore.brand}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        {/* <div className="form-field">
-          <label htmlFor="model">Model</label>
-          <input
-            type="text"
-            id="model"
-            value={newScore.model}
+            type="number"
+            id="electric"
+            value={newScore.electric}
             onChange={handleInputChange}
           />
         </div>
 
         <div className="form-field">
-          <label htmlFor="description">Description:</label>
+          <label htmlFor="gas">How much was your gas bill this month?</label>
           <input
-            type="text"
-            id="description"
-            value={newScore.description}
+            type="number"
+            id="gas"
+            value={newScore.gas}
             onChange={handleInputChange}
           />
-        </div> */}
+        </div>
 
-        <br />
-        <input className="submit-button" type="submit" />
+        <div className="form-field">
+          <label htmlFor="carMileage">
+            How much was your car mileage this year?
+          </label>
+          <input
+            type="number"
+            id="carMileage"
+            value={newScore.carMileage}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-field">
+          <label htmlFor="short_flights">
+            How many the number of short flights you’ve taken in the past year?
+            (4 hours or less)
+          </label>
+          <input
+            type="number"
+            id="short_flights"
+            value={newScore.short_flights}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="form-field">
+          <label htmlFor="long_flights">
+            How many the number of long flights you’ve taken in the past year?
+            (4 hours or more)
+          </label>
+          <input
+            type="number"
+            id="long_flights"
+            value={newScore.long_flights}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="form-field">
+          <label htmlFor="recycle_newspaper">Have you recycle newspaper?</label>
+          <input
+            type="checkbox"
+            id="recycle_newspaper"
+            value={newScore.recycle_newspaper}
+            onChange={handleCheckboxChange}
+          />
+        </div>
+        <div className="form-field">
+          <label htmlFor="recycle_aluminium">
+            Have you recycle aluminum and tin?
+          </label>
+          <input
+            type="checkbox"
+            id="recycle_aluminium"
+            value={newScore.recycle_aluminum}
+            onChange={handleCheckboxChange}
+          />
+        </div>
+
+        <button type="submit">Calculate my footprint</button>
       </form>
+
+      <br />
+
+      <div className="upsert-form">
+        <h2>Your footprint score is: {totalScore}</h2>
+        <p>
+          *Keep in mind that an “ideal” carbon footprint (or a “low” footprint)
+          is anywhere from 6,000 to 15,999 pounds per year. 16,000-22,000 is
+          considered average. Under 6,000 is considered very low. Over 22,000?
+          You may want to take some “living green” practices into consideration.
+        </p>
+        <p>
+          **This score is based on the "Environmental Equation" formula
+          developed by ecologist Alexandra Shimo-Barry.
+        </p>
+        <h4>
+          Invite your friends and join the competition for the care of the
+          planet, click here:
+        </h4>
+        <br />
+        <button onClick={handleShareScore}>Compete Now</button>
+      </div>
     </div>
   );
-  
-    
-    
-    
-    
 }
-
 
 // Multiply your monthly electric bill by 105
 
